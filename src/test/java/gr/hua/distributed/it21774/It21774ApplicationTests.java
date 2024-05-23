a46376d47e032e5cf22224339ba2c69f12585e1e
kevinstana <122367928+kevinstana@users.noreply.github.com>
2024-05-23 16:32:16 +0300

    added contract test and initial users

--- src/test/java/gr/hua/distributed/it21774/It21774ApplicationTests.java
+++ src/test/java/gr/hua/distributed/it21774/It21774ApplicationTests.java
@@ -4,14 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.annotation.Order;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
+import io.jsonwebtoken.Jwts;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,17 +22,17 @@ class It21774ApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
-	
+
+	@Value("${app.jwtSecret}")
+    private String jwtSecret;
+
 	@Test
-	@Order(1)
 	void contextLoads() {
 	}
 
+	// Login as admin to get jwt and create a new user
 	@Test
-	@Order(2)
-	public void createUserTest() throws Exception {
-
-		// Login as admin to get jwt and create a new user
+	public void a() throws Exception {
 		String adminJson = "{\"username\":\"admin\",\"password\":\"password\"}";
 
 		ResultActions adminResult = mockMvc.perform(post("/login")
@@ -39,7 +42,7 @@ class It21774ApplicationTests {
 		String jwt = adminResult.andReturn().getResponse().getHeader("Authorization");
 		adminResult.andExpect(status().isOk());
 
-		// Create the new user
+		// Create the new user (with notary role)
 		String userJson = "{\"username\":\"testuser\",\"password\":\"testpassword\",\"email\":\"test@hua.gr\",\"role\":[\"lawyer\"],\"firstName\":\"firstName\",\"lastName\":\"lastName\",\"afm\":\"777777777\",\"amka\":\"77777777777\"}";
 
 		ResultActions userResult = mockMvc.perform(post("/users")
@@ -50,10 +53,9 @@ class It21774ApplicationTests {
 		userResult.andExpect(status().isOk());
 	}
 
+	// Login as the new user
 	@Test
-	@Order(3)
-	public void newUserLoginTest() throws Exception {
-		// Login as the new user
+	public void b() throws Exception {
 		String userLoginJson = "{\"username\":\"testuser\",\"password\":\"testpassword\"}";
 
 		ResultActions userLoginResult = mockMvc.perform(post("/login")
@@ -61,5 +63,36 @@ class It21774ApplicationTests {
 				.content(userLoginJson));
 
 		userLoginResult.andExpect(status().isOk());
+	}
+
+	@Test
+	public void c() throws Exception {
+		String userLoginJson = "{\"username\":\"lawyer_one\",\"password\":\"password\"}";
+
+		ResultActions lawyerLoginResult = mockMvc.perform(post("/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(userLoginJson));
+
+		lawyerLoginResult.andExpect(status().isOk());
+
+		String jwt = lawyerLoginResult.andReturn().getResponse().getHeader("Authorization");
+		String payload = Jwts.parser()
+							.setSigningKey(jwtSecret)
+							.parseClaimsJws(jwt.split(" ")[1])
+							.getBody()
+							.toString();
+
+		String[] payloadToPieces = payload.split(",");
+		String[] payloadId = payloadToPieces[1].split("=");
+		Long userId = Long.parseLong(payloadId[1]);
+
+		String contractJson = "{\"afm\":[\"222222222\",\"333333333\",\"444444444\",\"555555555\"],\"text\":\"This is a test contract created by lawyer_one. Members of the contract are client one, two and lawyers one, two.\"}";
+
+		ResultActions userResult = mockMvc.perform(post("/users/" + userId + "/contract")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(contractJson)
+				.header("Authorization", jwt));
+
+		userResult.andExpect(status().isOk());
 	}
 }
