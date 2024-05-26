e710b3592c312a8b27511bba273fb016b13aa078
kevinstana <122367928+kevinstana@users.noreply.github.com>
2024-05-26 17:45:57 +0300

    Update data.sql

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,26 +1,26 @@
-INSERT INTO users (answer, afm, amka, email, first_name, last_name, password, username) VALUES
-    (null, '111111111', '11111111111', 'admin@gmail.com', 'Kevin', 'Stana', '$2a$10$VwKas4ss8uuLL.YpbycfXeT52yMjOXMce3OJe9wotGL4MT/Juo7tS', 'admin');
+-- INSERT INTO users (answer, afm, amka, email, first_name, last_name, password, username) VALUES
+--     (null, '111111111', '11111111111', 'admin@gmail.com', 'Kevin', 'Stana', '$2a$10$VwKas4ss8uuLL.YpbycfXeT52yMjOXMce3OJe9wotGL4MT/Juo7tS', 'admin');
 
-INSERT INTO users (answer, afm, amka, email, first_name, last_name, password, username) VALUES
-    (null, '222222222', '22222222222', 'client_one@gmail.com', 'Client', 'One', '$2a$10$VwKas4ss8uuLL.YpbycfXeT52yMjOXMce3OJe9wotGL4MT/Juo7tS', 'client_one');
+-- INSERT INTO users (answer, afm, amka, email, first_name, last_name, password, username) VALUES
+--     (null, '222222222', '22222222222', 'client_one@gmail.com', 'Client', 'One', '$2a$10$VwKas4ss8uuLL.YpbycfXeT52yMjOXMce3OJe9wotGL4MT/Juo7tS', 'client_one');
 
-INSERT INTO users (answer, afm, amka, email, first_name, last_name, password, username) VALUES
-    (null, '333333333', '33333333333', 'client_two@gmail.com', 'Client', 'Two', '$2a$10$VwKas4ss8uuLL.YpbycfXeT52yMjOXMce3OJe9wotGL4MT/Juo7tS', 'client_two');
+-- INSERT INTO users (answer, afm, amka, email, first_name, last_name, password, username) VALUES
+--     (null, '333333333', '33333333333', 'client_two@gmail.com', 'Client', 'Two', '$2a$10$VwKas4ss8uuLL.YpbycfXeT52yMjOXMce3OJe9wotGL4MT/Juo7tS', 'client_two');
 
-INSERT INTO users (answer, afm, amka, email, first_name, last_name, password, username) VALUES
-    (null, '444444444', '44444444444', 'lawyer_one@gmail.com', 'Lawyer', 'One', '$2a$10$VwKas4ss8uuLL.YpbycfXeT52yMjOXMce3OJe9wotGL4MT/Juo7tS', 'lawyer_one');
+-- INSERT INTO users (answer, afm, amka, email, first_name, last_name, password, username) VALUES
+--     (null, '444444444', '44444444444', 'lawyer_one@gmail.com', 'Lawyer', 'One', '$2a$10$VwKas4ss8uuLL.YpbycfXeT52yMjOXMce3OJe9wotGL4MT/Juo7tS', 'lawyer_one');
 
-INSERT INTO users (answer, afm, amka, email, first_name, last_name, password, username) VALUES
-    (null, '555555555', '55555555555', 'lawyer_two@gmail.com', 'Lawyer', 'Two', '$2a$10$VwKas4ss8uuLL.YpbycfXeT52yMjOXMce3OJe9wotGL4MT/Juo7tS', 'lawyer_two');
+-- INSERT INTO users (answer, afm, amka, email, first_name, last_name, password, username) VALUES
+--     (null, '555555555', '55555555555', 'lawyer_two@gmail.com', 'Lawyer', 'Two', '$2a$10$VwKas4ss8uuLL.YpbycfXeT52yMjOXMce3OJe9wotGL4MT/Juo7tS', 'lawyer_two');
 
-INSERT INTO role (role) VALUES
-('ROLE_ADMIN'),
-('ROLE_LAWYER'),
-('ROLE_NOTARY'),
-('ROLE_CLIENT');
+-- INSERT INTO role (role) VALUES
+-- ('ROLE_ADMIN'),
+-- ('ROLE_LAWYER'),
+-- ('ROLE_NOTARY'),
+-- ('ROLE_CLIENT');
 
-INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
-INSERT INTO user_role (user_id, role_id) VALUES (2, 4);
-INSERT INTO user_role (user_id, role_id) VALUES (3, 4);
-INSERT INTO user_role (user_id, role_id) VALUES (4, 2);
-INSERT INTO user_role (user_id, role_id) VALUES (5, 2);
+-- INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
+-- INSERT INTO user_role (user_id, role_id) VALUES (2, 4);
+-- INSERT INTO user_role (user_id, role_id) VALUES (3, 4);
+-- INSERT INTO user_role (user_id, role_id) VALUES (4, 2);
+-- INSERT INTO user_role (user_id, role_id) VALUES (5, 2);
